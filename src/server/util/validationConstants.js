028a4322bc8b817cd0dda207b152e531a8ba5ac5
omarraf <omarrafiqq@gmail.com>
2025-10-04 20:52:30 -0700

    create util file for constants

---
+++ src/server/util/validationConstants.js
@@ -0,0 +1,13 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const VALIDATION_CONSTANTS = {
+	// String length limits
+	PASSWORD_MAX_LENGTH: 1000,
+	TOKEN_MAX_LENGTH: 2000,
+	GENERAL_STRING_MAX_LENGTH: 1000,
+	SHORT_STRING_MAX_LENGTH: 256
+};
+
+module.exports = VALIDATION_CONSTANTS;
