7672cdf6db796926b9023bfc0ea4975aeb077113
Andrew Bonner <andrew.bonner@bsu.edu>
2025-11-20 16:58:32 -0500

    added more to the user routes test and made a TestUser class

--- src/server/test/routes/testUsers.js
+++
@@ -1,5 +0,0 @@
-import bcrypt from "bcryptjs/dist/bcrypt";
-import User from "../../models/User";
-class TestUsers {
-	static ADMIN;
-}
--- src/server/test/routes/userRoutesTests.js
+++ src/server/test/routes/userRoutesTests.js
@@ -13,43 +13,13 @@ const fs = require("fs");
 const User = require("../../models/User");
 const Configfile = require("../../models/obvius/Configfile");
 const bcrypt = require("bcryptjs");
+const { param } = require("../../app");
 
 const raw = fs.readFileSync("src/server/test/routes/routes.json", "utf8");
 const routeData = JSON.parse(raw);
 
 mocha.describe("different user types trying several", () => {
-	let users;
 	//create users
-	mocha.before("create users and insert them into the database", async () => {
-		const conn = testDB.getConnection();
-		users = [
-			new User(
-				undefined,
-				"adminuser",
-				await bcrypt.hash("admin321#", 10),
-				User.role.ADMIN
-			),
-			new User(
-				undefined,
-				"obviususer",
-				await bcrypt.hash("obvius321#", 10),
-				User.role.OBVIUS
-			),
-			new User(
-				undefined,
-				"csvuser",
-				await bcrypt.hash("csv321#", 10),
-				User.role.CSV
-			),
-			new User(
-				undefined,
-				"exportuser",
-				await bcrypt.hash("export321#", 10),
-				User.role.EXPORT
-			),
-		];
-		//insert users into the database
-	});
 	//next loop through each route in the json file based on the current format as of 11/18
 	//for (const authType in routeData) {
 	//const methods = routeData[authType];
@@ -57,19 +27,13 @@ mocha.describe("different user types trying several", () => {
 	//const routeList = methods[method];
 	//for (const route of routeList) {
 	//try adminuser against the first route (splitting routes up first)
-
 	//for (let i = 0; i > 3; i++) {
 	mocha.it("logging in", async () => {
 		let res = await chai.request(app).post("/api/login").send({
 			username: testUser.username,
 			password: testUser.password,
 		});
-		token = res.body.token;
-		let res2 = await chai
-			.request(app)
-			.post("/api/conversions/addConversion")
-			.set("token", token);
-		expect(res2).to.have.status(400);
+		expect(res).to.have.status(200);
 	});
 	//}
 
@@ -77,3 +41,29 @@ mocha.describe("different user types trying several", () => {
 	//}
 	//}
 });
+class TestUsers {
+	constructor() {
+		this.users = {};
+	}
+
+	static async addConnection(conn) {
+		await this.createUsers(
+			"ADMIN",
+			"adminuser",
+			"admin321#",
+			User.role.ADMIN,
+			conn
+		);
+	}
+
+	async createUsers(key, username, password, role, connection) {
+		const hash = await bcrypt.hash(password, 10);
+		const user = new User(undefined, username, hash, role);
+		await user.insert(conn);
+		this.users[key] = user;
+		return user;
+	}
+	static get(key) {
+		return this.users[key];
+	}
+}
