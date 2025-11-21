97504d502b34ef36b366776bd2a7a36d6721f84c
Zach-O-Bates <zbates486@gmail.com>
2025-11-21 16:32:10 -0500

    Start iterating through get and post routes

--- src/server/test/routes/userRoutesTests.js
+++ src/server/test/routes/userRoutesTests.js
@@ -13,12 +13,19 @@ const fs = require("fs");
 const User = require("../../models/User");
 const Configfile = require("../../models/obvius/Configfile");
 const bcrypt = require("bcryptjs");
+const sharedBody = { message: "test" };
+const sharedQuery = {
+	logLimit:10,
+	timeInterval: "LAST_24_HOURS",
+	logTypes:"info"
+};
 
 const raw = fs.readFileSync("src/server/test/routes/routes.json", "utf8");
 const routeData = JSON.parse(raw);
 
-mocha.describe("different user types trying several", () => {
+mocha.describe("Admin GET + POST route tests", () => {
 	let token;
+
 	mocha.beforeEach("admin user logs in", async () => {
 		const conn = testDB.getConnection();
 
@@ -33,14 +40,30 @@ mocha.describe("different user types trying several", () => {
 			});
 		token = res.body.token;
 	});
-	mocha.it("conversion-array refresh", async () => {
-		const res2 = await chai
-			.request(app)
-			.post("/api/conversion-array/refresh")
-			.set("token", token);
-		expect(res2).to.have.status(200);
+
+	
+	routeData.admin.GET.forEach(route => {
+		mocha.it(`Pass ${route} should allow admin`, async () => {
+			const url = resolveParams(route);
+			const req = chai.request(app).get(url)
+				.set("token", token)
+				.query(sharedQuery);
+			const res = await req.send(sharedBody);
+			expect(res.status).to.be.oneOf([200, 202]);
+		});
+	});
+
+	routeData.admin.POST.forEach(route => {
+		mocha.it(`Pass ${route} should allow admin`, async () => {
+			const url = resolveParams(route);
+			const req = chai.request(app).post(url)
+				.set("token", token);
+			const res = await req.send(sharedBody);
+			expect(res.status).to.be.oneOf([200, 202]);
+		});
 	});
 });
+	
 class TestUsers {
 	static adminPassword = "admin321#";
 	static async admin() {
@@ -52,3 +75,10 @@ class TestUsers {
 		);
 	}
 }
+
+function resolveParams(route) {
+    return route.replace(/:([A-Za-z_]+)/g, "1");
+}
+
+
+
